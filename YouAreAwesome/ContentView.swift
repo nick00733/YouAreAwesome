208e1caa65a3155e3084e2ce3e025759b2a9757f
Nick Nedjat <nick.nedjat@icloud.com>
2026-01-17 18:04:42 -0500

    Update after Video Session 1.2

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var message = "I Am A Programmer!"
     var body: some View {
         
         VStack {
@@ -16,10 +17,13 @@ struct ContentView: View {
                 .scaledToFit()
                 .foregroundStyle(.orange)
             
-            Text("I am a developer!")
+            Text(message)
                 .font(.largeTitle)
                 .fontWeight(.heavy)
                 .foregroundStyle(.red)
+            Button("Click me!") {
+                message = "Awesome!"
+            }
         }
         .padding()
         
