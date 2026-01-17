f57d69f02fd2dd7eadb49eaf7373611b252038f0
Nick Nedjat <nick.nedjat@icloud.com>
2026-01-17 14:58:49 -0500

    Update after first Lesson

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -16,7 +16,7 @@ struct ContentView: View {
                 .scaledToFit()
                 .foregroundStyle(.orange)
             
-            Text("You Are Awesome!")
+            Text("I am a developer!")
                 .font(.largeTitle)
                 .fontWeight(.heavy)
                 .foregroundStyle(.red)
